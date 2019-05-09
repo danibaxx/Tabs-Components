1b5ab286ebe8993f18d8d540ffd0b605db71376a
Danielle B <danibaxx@hotmail.com>
2019-05-09 17:47:43 -0600

    updated Dropdown.js with assignment to reference on line 11, now has functionality and working beautifully

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -6,11 +6,10 @@ class Dropdown {
     // console.log(this.element);
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
     this.button = this.element.querySelector('.dropdown-button');
-      // console.log(this.button);
+      console.log(this.button);
     
     // assign the reference to the ".dropdown-content" class found in the dropdown element
-    this.content = this.dropdown-content;
-    console.log(this.content);
+    this.content = document.querySelector('.dropdown-content');
 
     // Add a click handler to the button reference and call the toggleContent method.
     this.button.addEventListener('click', () => this.toggleContent()); {
@@ -21,11 +20,11 @@ class Dropdown {
   toggleContent() {
     
     // Toggle the ".dropdown-hidden" class off and on
-    this.content.classList.toggle('dropdown');
+    this.content.classList.toggle('dropdown-hidden');
     // this.content.classList.toggle('dropdow-hidden');
   }
 }
 
 
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
-let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
+let dropdowns = document.querySelectorAll('.dropdown').forEach(dropdown => new Dropdown(dropdown));
